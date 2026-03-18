9e8e7bcd550ac65c7b1a8dd95e1a34e6861879b1
kinitec33 <kininla7@gmail.com>
2026-03-18 03:40:34 +0100

    initial commit with routing setup

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { Routes, Route, Link } from "react-router-dom"
+
+function Home() {
+  return <h2>Home Page - Quotes Coming Soon</h2>
+}
+
+function Favorites() {
+  return <h2>Favorites Page</h2>
+}
+
+function About() {
+  return <h2>About Page</h2>
+}
+
+function App() {
+  return (
+    <div>
+      <nav style={{ display: "flex", gap: "20px" }}>
+        <Link to="/">Home</Link>
+        <Link to="/favorites">Favorites</Link>
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
